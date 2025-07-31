48e117b30446c3ec7ab958a7de8df8b6826bb732
Klevis-Pllumbi <pllumbiklevis1@gmail.com>
2025-07-31 16:14:05 +0200

    Init

---
+++ klosismart/src/main/java/com/klevispllumbi/klosismart/KlosismartApplication.java
@@ -0,0 +1,13 @@
+package com.klevispllumbi.klosismart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlosismartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlosismartApplication.class, args);
+	}
+
+}
---
+++ klosismart/src/test/java/com/klevispllumbi/klosismart/KlosismartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klevispllumbi.klosismart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlosismartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
